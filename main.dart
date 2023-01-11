e152af5c2297c9f4afc11d2282ff75d43355c486
유민기 <mgyou@nextree.io>
2023-01-11 21:40:07 +0800

    variables_2

--- main.dart
+++ main.dart
@@ -1,18 +1,9 @@
 void main() {
-  var name1 = '민기';
-  String name2 = '민기';
+  String? minking = 'minking';
+  minking = null;
 
-  var dynaName;
-  dynaName = 'minking';
-  dynaName = 12;
-  dynaName = true;
-
-  dynamic name;
-  // define name as String
-  if (name is String) {
-    // now code know name is String -> shows many methods on option
-    // name.isEmpty
-    // name.hashCode
+  // minking.length;
+  if (minking != null) {
+    minking.length;
   }
-  // outside of if - code doesn't know name's type - no many methods
 }
