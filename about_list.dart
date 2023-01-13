f016c46c1136a3a2f14d6db435748de5e7615a60
유민기 <mgyou@nextree.io>
2023-01-13 22:38:39 +0800

    collection if

---
+++ about_list.dart
@@ -0,0 +1,23 @@
+void main() {
+  //use var when possible
+  var giveMeFive = true;
+  var numbers = [
+    1,
+    2,
+    3,
+    4,
+    //collection if
+    if (giveMeFive) 5,
+    /*
+    same with
+    if (giveMeFice) {
+      numbers.add(5);
+    }
+    */
+  ];
+  // List<int> numbers2 = [1, 2, 3, 4];
+
+  print(numbers);
+
+  //support collection if, for
+}
